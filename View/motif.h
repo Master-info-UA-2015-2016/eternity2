c6eb7959a63d17be571ffb90f671f9e65d223bbb
flodavid <fl.david.53@gmail.com>
2016-02-25 17:12:36 +0100

    Réupload motif.h

--- View/motif.h
+++ View/motif.h
@@ -1,19 +1,13 @@
 #ifndef MOTIF_H
 #define MOTIF_H
 
-#include <QColor>
+#include <QColor> 
 
 class Motif
 {
-private:
-    int id;
-    QColor* color_int;
-    QColor* color_ext;
-
 public:
     Motif();
-
-//    TODO @Antoine
+//    TODO @Antoine (@AsCris)
 //    QColor* get_color_int();
 //    QColor* get_color_ext();
 };
