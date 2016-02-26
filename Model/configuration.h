bcb14ef55c86bd81c7880b96d1417bf834206d7b
Seymour49 <ugo.rayer@laposte.net>
2016-02-26 11:54:27 +0100

    config

--- Model/configuration.h
+++ Model/configuration.h
@@ -1,10 +1,33 @@
 #ifndef CONFIGURATION_H
 #define CONFIGURATION_H
 
-class Configuration
+#include "instance_model.h"
+
+/**
+ * Classe définissant une configuration, qui correspond à un ensemble
+ * de position ( case, rotation ) pour une instance donnée. La classe
+ * hérite d'instance car une instance peut disposer de plusieurs configurations
+ * mais une configuration dépend d'une et une seule instance.
+ *
+ * @brief The Configuration class
+ * @author Ugo
+ */
+
+
+class Configuration : public Instance
 {
+private:
+    std::vector<pair<int, int>> vectPosition;
 public:
+
+    /**
+     * Constructeur
+     */
     Configuration();
+
+    /**
+      * Getters et Setters, ne pas hésiter à en faire plein
+      */
 };
 
 #endif // CONFIGURATION_H
