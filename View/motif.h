1ebd43324d4d329a2999b642bee54a07975abea9
Ascris <antoinegarnier@hotmail.fr>
2016-02-29 12:19:21 +0100

    Couleurs en cours de changement

--- View/motif.h
+++ View/motif.h
@@ -3,18 +3,28 @@
 
 #include <QColor> 
 
+//couleurs utilisables
+enum AvailableColors{Black, Gray, Brown, Orange, Pink, Red, Yellow, LightGreen, DarkGreen, LightBlue, DarkBlue, Purple};
+
 class Motif
 {
 private:
-    int id;
     QColor* color_int;
     QColor* color_ext;
 
 public:
     Motif();
+    Motif(QColor* inte, QColor* exte);
+
+    void set_color_int(QColor* col);
+    void set_color_ext(QColor* col);
+
 //    TODO @Antoine (@AsCris)
-//    QColor* get_color_int();
-//    QColor* get_color_ext();
+    QColor* get_color_int();
+    QColor* get_color_ext();
+
+//definition de 2 couleurs propres a un motif en fonction de son id
+    void set_colors(int color_1, int color_2);
 };
 
 #endif // MOTIF_H
