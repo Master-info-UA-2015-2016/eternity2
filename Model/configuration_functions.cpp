21c22b2cae656e0f2c7dd133125d6109cca574df
flodavid <fl.david.53@gmail.com>
2016-03-14 18:27:40 +0100

    code avec canBePlaced mis en commentaire

--- Model/configuration_functions.cpp
+++ Model/configuration_functions.cpp
@@ -359,12 +359,12 @@ bool Configuration::tryPlaceAtEnd(Piece &piece)
     // TODO tester les 4 rotations
     int rotation;
     for (rotation= 0; rotation < 4 && !is_placed; ++rotation){
-        if ( canBePlaced(piece, rotation) ) {   // TODO faire
-            // Elle peut être ajoutée, donc on la place
-            placePiece(piece, rotation);
-            // La pièce est placée, cela entraîne la sortie de la boucle
-            is_placed= true;
-        }
+//        if ( canBePlaced(piece, rotation) ) {   // TODO à faire
+//            // Elle peut être ajoutée, donc on la place
+//            placePiece(piece, rotation);
+//            // La pièce est placée, cela entraîne la sortie de la boucle
+//            is_placed= true;
+//        }
     }
 
 #if DEBUG_CSP
